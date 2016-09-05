a7ace56112811ba976d8df72a8428276e0fc9fae
Amol Chavhan <amol3800@gmail.com>
2016-09-05 11:14:37 +0530

    Revert "Simple paint"
    This reverts commit c3cf9bf0d801a95d8a4c3fedbb671653eaf85859.

--- app/src/main/java/amol/firstcustomview/MainActivity.java
+++ app/src/main/java/amol/firstcustomview/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //   setContentView(new HeartbeatView(this));
+        setContentView(new HeartbeatView(this));
 
     }
 
